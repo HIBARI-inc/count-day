5c1d29ccc7ccf9a45e28d14501d0480b3de43194
nakano0328 <n.mitsu.0328@icloud.com>
2024-11-26 00:05:32 +0900

    configファイルの変更

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+import { NextConfig } from "next";
+
+// Next.jsの設定
+const nextConfig: NextConfig = {
   reactStrictMode: true,
+  //basePath: "/nextjs", // 基本パスの設定
+  images: {
+    domains: ["example.com"], // 画像のホスティング元を許可
+  },
+  env: {
+    CUSTOM_ENV_VAR: process.env.CUSTOM_ENV_VAR, // 環境変数を公開する場合に使用
+  },
 };
 
-module.exports = nextConfig;
+// 設定をエクスポート
+export default nextConfig;
